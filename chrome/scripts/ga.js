ab60489c9c08cc25c8769f21edc4ccc2f923d449
Sean Roberts <seanroberts@seans-mbp.attlocal.net>
2015-04-18 11:59:34 -0400

    updating Analytics with a uid

--- chrome/scripts/ga.js
+++ chrome/scripts/ga.js
@@ -2,7 +2,11 @@
     GOOGLE ANALYTICS
 */
 var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-60306007-1']);
+
+// analytics sessions opened and killed everytime you open the popup. 
+// passing the extension id -- not personally identifiable  -- should allow us
+// to track usage better.
+_gaq.push(['_setAccount', 'UA-60306007-1'], { userId: chrome.runtime.id });
 _gaq.push(['_trackPageview']);
 
 (function() {
